7287d1a84176d82a5057f8ed5e5861abbd920b3d
Fernando dos Santos Batista <fernandodossantos005@gmail.com>
2023-12-08 09:29:17 -0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(("login/", include('knox.urls'))
+    path('login/', include('knox.urls')),  # Correção aqui
 ]
