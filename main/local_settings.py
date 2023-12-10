dcd6f7f165608d1700cfd945fdf4c6594257c697
FernandodotDev <fernandodossantos005@gmail.com>
2023-12-10 10:56:48 -0300

    admin

--- main/local_settings.py
+++ main/local_settings.py
@@ -89,12 +89,27 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '1c42Cbgf-dA56D3E51g2DdedE5ebbAEf',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '36673',
+    }
+}
+
+
+
+# Database SQLITE
+""""
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
