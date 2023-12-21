42195623d55bfb7ca35224e0177a1610cbf40ac4
Fernando dos Santos Batista <fernandodossantos005@gmail.com>
2023-12-21 08:23:29 -0300

    Update serializers.py

--- app/serializers.py
+++ app/serializers.py
@@ -5,13 +5,13 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ('username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
         
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
-            # email=validated_data['email'],
+            email=validated_data['email'],
             password=validated_data['password']
             
         )
